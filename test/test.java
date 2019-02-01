80540231a1e17f4e64ed7fd700370195b1b94ab6
Vera Blomkvist Karlsson <verakar@kth.se>
2019-02-01 10:22:52 +0100

    Add test file

---
+++ test/test.java
@@ -0,0 +1,15 @@
+import junit.org.*;
+import static org.junit.Assert.*;
+
+public class Test {
+
+    @Test
+    public void testSum() {
+        assertTrue(true);
+    }
+    @Test
+    public void testSum2() {
+        assertFalse(false);
+    }
+
+}
